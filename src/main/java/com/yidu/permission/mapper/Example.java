b7068efe68192ae96e77778afd47e20f479df467
小财 <1446639902@qq.com>
2020-08-31 20:31:53 +0800

    NGU-FA金融估值核算系统Demo

--- src/main/java/com/yidu/permission/mapper/Example.java
+++ src/main/java/com/yidu/permission/mapper/Example.java
@@ -1,4 +1,10 @@
 package com.yidu.permission.mapper;
 
-public class Example {
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface Example {
+    public List selectInfo();
 }
